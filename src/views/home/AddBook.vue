ccbe946f4b2c8c915c694bad7351f528932be192
Miguel Perico <mgledo@gmail.com>
2021-10-04 22:21:12 -0500

    Vista añadir libro actualizada

--- src/views/home/AddBook.vue
+++ src/views/home/AddBook.vue
@@ -1,72 +1,57 @@
 <template>
   <v-container fluid >
-      <h1>vista del addbook</h1>
+    <h3>Añadir libro a biblioteca</h3>
       <v-row align-content="center" justify="center">
-          <v-col>
+        <v-col>
+          <v-form
+          ref="form"
+          v-model="valid"
+          @submit.prevent="addBook()"
+          >
+            <v-text-field
+              v-model="form.title"
+              placeholder="Título, ISBN o Autor"
+              :counter="10"        
+              label="Buscar libro"
+              required
+            ></v-text-field>            
 
-    <v-form
-    ref="form"
-    v-model="valid"
-    @submit.prevent="addBook()"
-  >
-    <v-text-field
-      v-model="form.title"
-      :counter="10"
+            <!-- <v-checkbox
+              
+              label="Do you agree?"
+              required
+            ></v-checkbox> -->
+
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              type="submit"
+            >
+              Buscar
+            </v-btn>
+
+            <v-btn
+              color="error"
+              class="mr-4"
+              @click="reset"
+            >
+              Limpiar formulario
+            </v-btn>
+
+            <!-- <v-btn
+              color="warning"
+              @click="resetValidation"
+            >
+              Reset Validation
+            </v-btn> -->
+        </v-form>
+      </v-col>
+    </v-row>
+    <v-row>
       
-      label="titulo del Libro"
-      required
-    ></v-text-field>
 
-    <v-text-field
-      v-model="form.isbn"
-      label="ISBN"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="form.author"
-      label="Autor"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="form.imgStatus"
-      label="Cargar Imagenes"
-      required
-    ></v-text-field>
-
-    <v-checkbox
-      
-      label="Do you agree?"
-      required
-    ></v-checkbox>
-
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      type="submit"
-    >
-      Validate
-    </v-btn>
-
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset Form
-    </v-btn>
-
-    <v-btn
-      color="warning"
-      @click="resetValidation"
-    >
-      Reset Validation
-    </v-btn>
-  </v-form>
-          </v-col>
-      </v-row>
-      
-       
+    </v-row>
   </v-container>
 </template>
 
